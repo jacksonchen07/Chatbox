558e8ccb7362af981c0ef41f9c61b5949d6ccc62
Jackson Chen <jackson.chen@leidos.com>
2022-01-20 15:17:35 -0600

    Made comments to first inital commit

--- client.adb
+++ client.adb
@@ -1,3 +1,6 @@
+-- Examples and useage of sockets in Ada https://en.wikibooks.org/wiki/Ada_Programming/Libraries/GNAT.Sockets
+-- https://rosettacode.org/wiki/Sockets#Ada
+
 with GNAT.Sockets; use GNAT.Sockets;
 with Ada.Text_IO;
 
@@ -6,19 +9,23 @@ procedure client is
     Address : Sock_Addr_Type;
     Channel : Stream_Access;
 begin
-    Initialize;
+    Initialize; -- Must be called before socket routine
     Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
     Address.Port := 1_024;
     Create_Socket (Socket => Client);
 
     delay 0.2;
 
-    Connect_Socket (Client, Address);
-    Channel := Stream (Client);
-    String'Output (Channel, "Hello Socket World");
+    Connect_Socket
+       (Client,
+        Address); -- Connect and Autmoatically bind to an address since localhost is the server
+    Channel := Stream (Client); -- Stream associated to the socket
+    String'Output
+       (Channel, "Hello Socket World, Server!"); -- Sends message to stream
     delay 0.2;
 
-    Ada.Text_IO.Put_Line (String'Input (Channel));
+    Ada.Text_IO.Put_Line
+       (String'Input (Channel)); -- Prints any message recevied by server
 
     Close_Socket (Client);
 end client;
--- server.adb
+++ server.adb
@@ -9,26 +9,31 @@ procedure server is
     Socket  : Socket_Type;
     Channel : Stream_Access;
 begin
-    Initialize (Process_Blocking_IO => False);
-    Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
+    Initialize
+       (Process_Blocking_IO => False); -- Must be called before socket routine
+    Address.Addr := -- Get internet address of host. (Localhost here)
+       Addresses (Get_Host_By_Name (Host_Name), 1);
     Address.Port := 1_024;
 
-    Create_Socket (Server);
+    Create_Socket
+       (Server); -- Socket will need to be associated with an address on server sockets
     Set_Socket_Option (Server, Socket_Level, (Reuse_Address, True));
-    Bind_Socket (Server, Address);
-    Listen_Socket (Server);
-    Accept_Socket (Server, Socket, Address);
+    Bind_Socket (Server, Address); -- Bind Socket
+    Listen_Socket (Server); -- Start listening for any incoming sockets
+    Accept_Socket (Server, Socket, Address); -- Accept any incoming sockets
 
-    Channel := Stream (Socket);
+    Channel := Stream (Socket); -- Returns a stream from the incoming socket
 
     delay 0.2;
 
     declare
-        Message : String := String'Input (Channel);
+        Message : String :=
+           String'Input (Channel); -- Recieves message from associated socket
     begin
         Ada.Text_IO.Put_Line (Message);
 
-        String'Output (Channel, Message);
+        -- Send same message back to associated socket
+        String'Output (Channel, "Hello Socket World, Client!");
     end;
 
     Close_Socket (Server);
