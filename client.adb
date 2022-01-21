dab538c756bcc374decce8a9d59b5c249fb23038
BKambic <98051852+BKambic@users.noreply.github.com>
2022-01-21 14:28:13 -0600

    Update client.adb

--- client.adb
+++ client.adb
@@ -8,11 +8,13 @@ procedure client is
     Client  : Socket_Type;
     Address : Sock_Addr_Type;
     Channel : Stream_Access;
+    Input : String; -- takes input for the string 
 begin
     Initialize; -- Must be called before socket routine
     Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
     Address.Port := 1_024;
     Create_Socket (Socket => Client);
+    Get(Input);
 
     delay 0.2;
 
@@ -21,7 +23,7 @@ begin
         Address); -- Connect and Autmoatically bind to an address since localhost is the server
     Channel := Stream (Client); -- Stream associated to the socket
     String'Output
-       (Channel, "Hello Socket World, Server!"); -- Sends message to stream
+       (Channel, Input); -- Sends message to stream (Modifed this so it sends user Input)
     delay 0.2;
 
     Ada.Text_IO.Put_Line
