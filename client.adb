48a96257c90e2b9e4efa2327c3e2e0c4dcd5f5c4
BKambic <98051852+BKambic@users.noreply.github.com>
2022-01-24 15:22:57 -0600

    Update client.adb

--- client.adb
+++ client.adb
@@ -8,9 +8,9 @@ procedure client is
     Client  : Socket_Type;
     Address : Sock_Addr_Type;
     Channel : Stream_Access;
-    User_Input : string; -- takes input for the string 
+    User_Input : String(1..2) := (others => ' ');-- takes input for the string 
 begin
-    Put_Line ("Pease enter a number 1-5");
+    Ada.Text_IO.Put_Line ("Pease enter a number 1-5");
     Get(User_Input);
 
     Initialize; -- Must be called before socket routine
@@ -28,7 +28,7 @@ begin
        (Client,
         Address); -- Connect and Autmoatically bind to an address since localhost is the server
     Channel := Stream (Client); -- Stream associated to the socket
-    If Input = '1' then -- added if loop to check for valid input
+    If Input = 1 then -- added if loop to check for valid input
     String'Output
        (Channel, User_input); -- Sends message to stream (Modifed this so it sends user Input)
     delay 0.2;
@@ -36,7 +36,7 @@ begin
     Ada.Text_IO.Put_Line
        (String'Input (Channel)); 
     
-    elsif Input = '2' then
+    elsif Input = 2 then
     String'Output
        (Channel, User_Input); 
     delay 0.2;
@@ -44,7 +44,7 @@ begin
     Ada.Text_IO.Put_Line
        (String'Input (Channel)); 
      
-    elsif Input = '3' then
+    elsif Input = 3 then
     String'Output
        (Channel, User_Input); 
     delay 0.2;
@@ -53,7 +53,7 @@ begin
        (String'Input (Channel)); 
 
 
-    elsif Input = '4'
+    elsif Input = 4
     then
     String'Output
        (Channel, User_Input); 
@@ -62,7 +62,7 @@ begin
     Ada.Text_IO.Put_Line
        (String'Input (Channel)); 
        
-           elsif Input = '5' then
+           elsif Input = 5 then
     String'Output
        (Channel, User_Input); 
     delay 0.2;
@@ -70,7 +70,7 @@ begin
     Ada.Text_IO.Put_Line
        (String'Input (Channel)); 
   else
-  Put_Line ("Error invalid Input"); -- invalid input error
+  Ada.Text_IO.Put_Line  ("Error invalid Input"); -- invalid input error
   end if;
   
     Close_Socket (Client);
