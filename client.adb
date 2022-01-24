0770639a26d39c972cea16d6f633eced5569eef4
BKambic <98051852+BKambic@users.noreply.github.com>
2022-01-24 15:37:31 -0600

    Update client.adb

--- client.adb
+++ client.adb
@@ -10,8 +10,7 @@ procedure client is
     Channel : Stream_Access;
     User_Input : String(1..2) := (others => ' ');-- takes input for the string 
 begin
-    Ada.Text_IO.Put_Line ("Pease enter a number 1-5");
-    Get(User_Input);
+
 
     Initialize; -- Must be called before socket routine
     Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
@@ -23,12 +22,19 @@ begin
 
 
     delay 0.2;
+    
+
 
     Connect_Socket
        (Client,
         Address); -- Connect and Autmoatically bind to an address since localhost is the server
     Channel := Stream (Client); -- Stream associated to the socket
-    If Input = 1 then -- added if loop to check for valid input
+    
+    
+    Ada.Text_IO.Put_Line ("Pease enter a number 1-5");
+    Get(User_Input);
+    
+    If  User_Input = 1 then -- added if loop to check for valid input
     String'Output
        (Channel, User_input); -- Sends message to stream (Modifed this so it sends user Input)
     delay 0.2;
@@ -36,7 +42,7 @@ begin
     Ada.Text_IO.Put_Line
        (String'Input (Channel)); 
     
-    elsif Input = 2 then
+    elsif  User_Input = 2 then
     String'Output
        (Channel, User_Input); 
     delay 0.2;
@@ -44,7 +50,7 @@ begin
     Ada.Text_IO.Put_Line
        (String'Input (Channel)); 
      
-    elsif Input = 3 then
+    elsif  User_Input = 3 then
     String'Output
        (Channel, User_Input); 
     delay 0.2;
@@ -53,7 +59,7 @@ begin
        (String'Input (Channel)); 
 
 
-    elsif Input = 4
+    elsif  User_Input = 4
     then
     String'Output
        (Channel, User_Input); 
