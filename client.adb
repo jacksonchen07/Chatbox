6af528570850a2ff4d62cd2f577552ea03f461e2
curzsh <nstaub91@gmail.com>
2022-01-25 17:40:21 -0600

    Updates to take user input and send to the server.

--- client.adb
+++ client.adb
@@ -5,14 +5,11 @@ With Ada.Integer_Text_IO;
 with Ada.Strings;
 
 procedure client is
-  type Max_String is new standard.string  (1..264);-- takes input for the string 
     Client  : Socket_Type;
     Address : Sock_Addr_Type;
     Channel : Stream_Access;
-   User_Input : Max_String;
+    User_Input : String := Ada.Text_IO.Get_Line;
 begin
-   Ada.Text_IO.Get (User_Input);
-
 
     Initialize; -- Must be called before socket routine
     Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
@@ -24,9 +21,12 @@ begin
     Connect_Socket
        (Client,
         Address); -- Connect and Autmoatically bind to an address since localhost is the server
+   
     Channel := Stream (Client); -- Stream associated to the socket
-    Max_String'Output
-       (Channel, User_Input); -- Sends mes    sage to stream
+   
+    String'Output
+       (Channel, User_Input); -- Sends message to stream
+   
     delay 0.2;
 
     Ada.Text_IO.Put_Line
