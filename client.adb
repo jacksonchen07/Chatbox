f9d20be8177f20f158bbf2fdcbf78750899115fe
Jackson Chen <jackson.chen@leidos.com>
2022-02-03 15:58:02 -0600

    Added name feature and adding port selection for client. Will fix more stuff tomorrow.

--- client.adb
+++ client.adb
@@ -1,8 +1,9 @@
 with Ada.Strings;
-with GNAT.Sockets;           use GNAT.Sockets;
-with Ada.Text_IO;            use Ada.Text_IO;
+with GNAT.Sockets;            use GNAT.Sockets;
+with Ada.Text_IO;             use Ada.Text_IO;
 with Ada.Integer_Text_IO;
-with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
+with Ada.Command_Line;        use Ada.Command_Line;
+with Ada.Characters.Latin_1;  use Ada.Characters.Latin_1;
 with Ada.Calendar;            use Ada.Calendar;
 with Ada.Calendar.Formatting; use Ada.Calendar.Formatting;
 
@@ -51,11 +52,16 @@ procedure Client is
 
         loop
             declare
-                User_Input : String := Get_Line;
-                Time_of_Message : Time := Clock; -- gets current time
+                User_Input      : String := Get_Line;
+                Time_of_Message : Time   := Clock; -- gets current time
             begin
                 -- exit when End_Of_File;
-                String'Output (Channel, User_Input & (" ") & Image (Time_of_Message)); -- Sends message to stream (Modified to display current time)
+                Put_Line ("message sent");
+                String'Output
+                   (Channel,
+                    User_Input & (" ") &
+                    Image
+                       (Time_of_Message)); -- Sends message to stream (Modified to display current time)
             end;
         end loop;
     end Send_Msg;
@@ -76,16 +82,37 @@ procedure Client is
     --     end loop;
     -- end Check_End;
 begin
+    -- Get port from user
+    if Argument_Count = 0 then
+        Put_Line (Current_Error, "Error - No port was given.");
+        Set_Exit_Status (Failure);
+    elsif Argument_Count > 1 then
+        Put_Line (Current_Error, "Error - Please only put one argument");
+        Set_Exit_Status (Failure);
+    else
 
-    Initialize; -- Must be called before socket routine
-    Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
-    Address.Port := 1_024;
-    Create_Socket (Socket => Client);
+        Initialize; -- Must be called before socket routine
+        Address.Addr := Addresses (Get_Host_By_Name (Host_Name), 1);
+        Address.Port := Port_Type (Integer'Value (Argument (1)));
+        Create_Socket (Socket => Client);
 
-    -- Connect and Autmoatically bind to an address since localhost is the server
-    Connect_Socket (Client, Address);
+        -- Connect and Autmoatically bind to an address since localhost is the server
+        Connect_Socket (Client, Address);
 
-    --Start Recieving and sending tasks
-    Rec_Msg.Start (Client);
-    Send_Msg.Start (Client);
+        Put_Line ("Server connected...");
+
+        Put_Line ("Please enter a Username");
+        declare
+            Username : String := Get_Line;
+        begin
+            -- Send username to server
+            String'Output (Stream (Client), Username);
+        end;
+
+        Put_Line ("Username sent");
+
+        --Start Recieving and sending tasks
+        Rec_Msg.Start (Client);
+        Send_Msg.Start (Client);
+    end if;
 end Client;
